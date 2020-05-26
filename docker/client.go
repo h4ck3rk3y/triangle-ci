3abfc3d0ab06aed7fb18236924ccc96a7f257cca
Gyanendra Mishra <anomaly.the@gmail.com>
2020-05-26 10:44:40 +0400

    can host things now (#6)

--- docker/client.go
+++ docker/client.go
@@ -34,3 +34,30 @@ func RunDockerFile(path string, id string, output *string) (status bool, err err
 	}
 	return true, nil
 }
+
+// ComposeUp ...
+func ComposeUp(path string, id string, output *string) (status bool, err error) {
+	_, err = git.GetDockerComposeFile((path))
+
+	if err != nil {
+		return false, errors.New("cant find docker compose")
+	}
+
+	cmd := exec.Command("docker-compose", "up")
+	cmd.Dir = path
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+
+	scanner := bufio.NewScanner(stdout)
+	scanner.Split(bufio.ScanLines)
+	var foo string
+	for scanner.Scan() {
+		foo = foo + "\n" + scanner.Text()
+		*output = foo
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
